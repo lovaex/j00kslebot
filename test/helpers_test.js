65e02cc005bacbf24bcdc3d2a94c370ffaa2dc8e
laserpez <laserpe.z@gmail.com>
2016-03-21 15:20:38 +0100

    Sends custom "not found" messages.

--- test/helpers_test.js
+++ test/helpers_test.js
@@ -6,11 +6,19 @@ chai.use(spies);
 var expect = chai.expect;
 describe("helpers", function() 
 {
-	describe("#tellMeSomething", function {
+	describe("#tellMeSomething", function () {
 		it("should return a Renzo-style random message", function()
 		{
 			var something = helpers.tellMeSomething();
 			expect(something).to.be.a('string');
+		})
+	});
+	describe("#notFoundMessage", function () {
+		it("should return a 'not found' answer to some human", function()
+		{
+			username = "U12345678";
+			var something = helpers.notFoundMessage(username);
+			expect(something).to.include(username)
 		})
 	});
 });

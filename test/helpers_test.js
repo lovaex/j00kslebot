4641a40a83bd33a8e14b185dfaea161426bcb554
laserpez <laserpe.z@gmail.com>
2016-03-22 09:17:17 +0100

    Minor changes.

--- test/helpers_test.js
+++ test/helpers_test.js
@@ -1,4 +1,5 @@
 
+var util = require('util');
 helpers = require("../lib/helpers");
 var chai = require('chai'), spies = require('chai-spies');
 chai.use(spies);
@@ -21,4 +22,17 @@ describe("helpers", function()
 			expect(something).to.include(username)
 		})
 	});
+	describe("#notFoundMessages", function () {
+		it("should return a 'not found' answer to some human", function()
+		{
+			username = "U03FYGH7K";
+			messages = helpers.notFoundMessages();
+			for (var elem of messages) {
+				something = util.format(elem, username);
+				expect(something).to.include(username);
+			}
+				
+		})
+	});
+
 });
